7d7499008ece28a108a2ec66dcb48ecd9bd74de0
hnhaefliger <henry_haefliger@protonmail.com>
2021-10-12 12:31:37 -0400

    getting pieces from peers

--- bittorrent/peer.py
+++ bittorrent/peer.py
@@ -1,10 +1,11 @@
 import socket
 import struct
 import time
+from . import messages
 
 class Peer:
-    def __init__(self, info_hash, peer_id, ip, port, n_pieces):
-        self. info_hash = info_hash
+    def __init__(self, info_hash, peer_id, ip, port, n_pieces, termination_callback=lambda x: 0):
+        self.info_hash = info_hash
         self.peer_id = peer_id
 
         self.am_choking = 1
@@ -12,26 +13,15 @@ class Peer:
         self.peer_choking = 1
         self.peer_interested = 0
 
-        self.pstrlen = 19
-        self.pstr = "BitTorrent protocol"
-
         self.ip = ip
         self.port = port
 
         self.n_pieces = n_pieces
         self.has = 0b0
 
-    def try_to_get(self, index, begin, length):
-        if self.has & 0b1 << index:
-            self.send(struct.pack('4s1s', b'0x0001', b'0x2'))
-            self.am_interested = 1
+        self.seeking = (0, 0, 0, 0, lambda x: 0, lambda x: 0, lambda x: 0)
 
-            if not(self.peer_choking):
-                self.send(struct.pack('4s1s', b'0x0001', b'0x1'))
-                self.am_choking = 0
-
-                self.send(struct.pack('4s1s4s4s4s', b'0x000d', b'0x6', index.to_bytes(4, 'big'), begin.to_bytes(4, 'big'), length.to_bytes(4, 'big')))
-                print(f'requested piece {index} from {self.ip}.')
+        self.termination_callback = termination_callback
 
     def mainloop(self):
         self.last_alive = time.time()
@@ -41,53 +31,17 @@ class Peer:
 
             if data:
                 if data[0]:
-                    if data[1] == 0:
-                        self.peer_choking = 1
-                        print(f'{self.ip} has started choking.')
-
-                    elif data[1] == 1:
-                        self.peer_choking = 0
-                        print(f'{self.ip} has stopped choking.')
-
-                    elif data[1] == 2:
-                        self.peer_interested = 1
-                        print(f'{self.ip} is interested.')
-
-                    elif data[1] == 3:
-                        self.peer_interested = 0
-                        print(f'{self.ip} is not interested.')
-
-                    elif data[1] == 4:
-                        index = int.from_bytes(data[2], 'big')
-
-                        print(f'{self.ip} has piece {index}.')
-                        
-                        if index < self.n_pieces:
-                            self.has |= 0b1 << index
-
-                    elif data[1] == 5:
-                        print(f'{self.ip} sent a bitfield.')
-
-                        if len(data[2]) <= self.n_pieces:
-                            self.has |= int.from_bytes(data[2], 'big')
-
-                        else:
-                            self.socket.close()
-                            exit()
-
-                    elif data[1] == 6:
-                        print(f'{self.ip} requested.')
-
-                    elif data[1] == 7:
-                        print(f'{self.ip} sent a piece.')
-
-                    elif data[1] == 8:
-                        print(f'{self.ip} cancelled.')
+                    if data[1] == 0: self.start_choking()
+                    elif data[1] == 1: self.stop_choking()
+                    elif data[1] == 2: self.start_interested()
+                    elif data[1] == 3: self.stop_interested()
+                    elif data[1] == 4: self.add_has(data[2])
+                    elif data[1] == 5: self.bitfield(data[2])
+                    elif data[1] == 6: self.requested_piece(data[2])
+                    elif data[1] == 7: self.receive_piece(data[2])
+                    elif data[1] == 8: self.cancel(data[2])
             
-            if self.last_alive - time.time() > 2 * 60:
-                print(f'keep alive was sent to {self.ip}.')
-                self.send(struct.pack('4s', b'0x0000'))
-                self.last_alive = time.time()
+            self.keepalive()
 
     def send(self, message):
         try:
@@ -95,10 +49,13 @@ class Peer:
             return True
 
         except:
+            self.socket.close()
+            self.termination_callback(self.ip)
             return False
 
     def recv(self):
         length, message_id, content = 0, 0, b''
+
         try:
             length = int.from_bytes(self.socket.recv(4), 'big')
 
@@ -115,22 +72,118 @@ class Peer:
 
     def handshake(self):
         try:
-            self.socket = socket.create_connection((self.ip, self.port), 2)
+            self.socket = socket.create_connection((self.ip, self.port), 3)
+            print(f'connected to {self.ip} on port {self.port}')
 
         except:
+            print(f'failed to connect to {self.ip} on port {self.port}')
+            self.termination_callback(self.ip)
             return False
 
-        handshake = struct.pack(">B{}s8s20s20s".format(self.pstrlen), self.pstrlen, self.pstr.encode('utf-8'), b'\x00'*8, self.info_hash, self.peer_id)
+        handshake = messages.pack_handshake(self.info_hash, self.peer_id)
         
         if self.send(handshake):
             try:
-                response = self.socket.recv(1 + 19 + 8 + 20 + 20)
-                response = struct.unpack(">B{}s8s20s20s".format(self.pstrlen), response)
-
+                response = messages.unpack_handshake(self.socket.recv(1 + 19 + 8 + 20 + 20))
                 return True
 
             except:
+                self.termination_callback(self.ip)
                 return False
             
         else:
+            self.termination_callback(self.ip)
             return False
+
+    def start_choking(self):
+        self.peer_choking = 1
+        print(f'{self.ip} has started choking.')
+
+    def stop_choking(self):
+        self.peer_choking = 0
+        print(f'{self.ip} has stopped choking.')
+
+        if self.seeking[0] == 1:
+            if self.send(messages.pack_request(self.seeking[1], self.seeking[2], self.seeking[3])):
+                print(f'requested piece {self.seeking[1]} from {self.ip}.')
+
+                self.seeking[5]((self.ip, self.seeking[1], self.seeking[2], self.seeking[3]))
+
+                self.seeking = (2, self.seeking[1], self.seeking[2], self.seeking[3], self.seeking[4], self.seeking[5], self.seeking[6])
+
+    def start_interested(self):
+        self.peer_interested = 1
+        print(f'{self.ip} is interested.')
+
+    def stop_interested(self):
+        self.peer_interested = 0
+        print(f'{self.ip} is not interested.')
+
+    def add_has(self, piece):
+        index = int.from_bytes(piece, 'big')
+
+        print(f'{self.ip} has piece {index}.')
+
+        if index < self.n_pieces:
+            self.has |= (0b1 << index)
+
+    def bitfield(self, bitfield):
+        print(f'{self.ip} sent a bitfield.')
+
+        if len(bitfield) <= self.n_pieces:
+            self.has |= int.from_bytes(bitfield, 'little')
+
+        else:
+            self.socket.close()
+            print(f'connection with {self.ip} terminated.')
+            exit()
+
+    def requested_piece(self, data):
+        
+        print(f'{self.ip} requested.')
+
+    def receive_piece(self, data):
+        print(f'{self.ip} sent a piece.')
+
+        if self.seeking[0] > 1:
+            self.seeking[6]((self.ip, self.seeking[1], self.seeking[2], self.seeking[3], data))
+            self.seeking = (0, 0, 0, 0, lambda x: 0, lambda x: 0, lambda x: 0)
+
+    def cancel_request(self, data):
+        print(f'{self.ip} cancelled.')
+
+    def keepalive(self):
+        if time.time() - self.last_alive > 110:
+            if self.send(messages.pack_keepalive()):
+                print(f'keep alive was sent to {self.ip}.')
+                self.last_alive = time.time()
+
+        time.sleep(0.05)
+
+    def get(self, index, begin, length, has_callback=lambda x: 0, getting_callback=lambda x: 0, got_callback=lambda x: 0):
+        if not(self.seeking[0]):
+            if self.has & (0b1 << index):
+                has_callback((self.ip, index, begin, length))
+
+                if self.send(messages.pack_interested()):
+                    self.am_interested = 1
+                    self.seeking = (1, index, begin, length, has_callback, getting_callback, got_callback)
+
+                    print(f'interested in {self.ip}.')
+
+                    if not(self.peer_choking):
+                        if self.send(messages.pack_request(index, begin, length)):
+                            print(
+                                f'requested piece {self.seeking[1]} from {self.ip}.')
+
+                            getting_callback((self.ip, index, begin, length))
+
+                            self.seeking = (2, index, begin, length, has_callback, getting_callback, got_callback)
+
+    def cancel(self, index, begin, length):
+        if self.seeking[1] == index and self.seeking[2] == begin and self.seeking[3] == length:
+            if self.seeking[0] > 0:
+                self.send(messages.pack_uninterested())
+
+                if self.seeking[0] > 1:
+                    self.send(messages.pack_cancel(self.seeking[1], self.seeking[2], self.seeking[3]))
--- bittorrent/torrent.py
+++ bittorrent/torrent.py
@@ -5,6 +5,7 @@ import threading
 import random
 import hashlib
 import math
+import time
 
 
 class Torrent:
@@ -26,8 +27,9 @@ class Torrent:
         self.left = len(self.pieces) * self.piece_length
 
         self.tracker = Tracker(data['announce'], self.info_hash, self.peer_id, 6969)
-        self.peers = []
-        self.downloaded_pieces = []
+        
+        self.peers = {}
+        self.threads = {}
 
     @staticmethod
     def open(path):
@@ -36,17 +38,33 @@ class Torrent:
 
         return Torrent(data)
 
+    def peer_failure_callback(self, ip):
+        try:
+            self.threads[ip].stop()
+            del self.threads[ip]
+            del self.peers[ip]
+            print(f'connection with {ip} terminated.')
+
+        except:
+            pass
+
     def start(self):
         peers = self.tracker.get_peers(self.uploaded, self.downloaded, self.left)
 
-        for peer in peers:
-            self.peers.append(Peer(self.info_hash, self.peer_id, peer['ip'], peer['port'], math.ceil(len(self.pieces) / 8)))
+        print(f'found {len(peers)} peers.')
 
-            if not(self.peers[-1].handshake()):
-                del self.peers[-1]
+        for peer in peers:
+            self.peers[peer['ip']] = Peer(self.info_hash, self.peer_id, peer['ip'], peer['port'], math.ceil(len(self.pieces) / 8), termination_callback=self.peer_failure_callback)
+
+            if not(self.peers[peer['ip']].handshake()):
+                del self.peers[peer['ip']]
 
             else:
-                threading.Thread(target=self.peers[-1].mainloop, daemon=False).start()
+                self.threads[peer['ip']] = threading.Thread(target=self.peers[peer['ip']].mainloop, daemon=True)
+                self.threads[peer['ip']].start()
 
         for peer in self.peers:
-            peer.try_to_get(0, 0, 2**14)
+            self.peers[peer].get(1, 1, 2**14)
+
+        while True:
+            time.sleep(1)
